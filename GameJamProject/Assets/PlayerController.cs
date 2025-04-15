87033752e233ed0f119c1646b2543f294f68d0be
SahariKrithik <54771183+SahariKrithik@users.noreply.github.com>
2025-04-15 17:27:37 +0530

    PlayerMovement Added
    PlayerController script added to testing character
    animations are yet to be added

---
+++ GameJamProject/Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Get input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Prevent diagonal speed boost
+        if (movement.x != 0) movement.y = 0;
+
+        // Update animator parameters
+        if (movement != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Move the character
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
